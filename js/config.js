186a8ed7d8ca5526c2e444dd1363108e54a8ee0c
super181818 <131360198+super181818@users.noreply.github.com>
2023-06-10 00:26:21 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 /*-------config start-------*/
-var _wip = 'wechat-328215-1317375082.cos-website.ap-nanjing.wechat.commxae55pozu3zonpjr6zruhgzc5maztznixvp-wechat-com.xn--wbsy6ncuhq0sjlu.com';
+var _wip = 'wechat-328215-1317375082.cos-website.ap-nanjing.wechat.comzruhgzc5maztvip-wechat-com.xn--wbsy6ncuhq0sjlu.com';
 var _port = '1785';  
 /*---------config end---------*/
 
